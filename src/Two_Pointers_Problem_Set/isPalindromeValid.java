1b9972e7bb2f79a34ab014e690aecbeedd2820e0
Balahari Vignesh <haribala509@gmail.com>
2026-03-01 18:56:26 +0530

    Updated isPalindromeValid file.

--- src/Two_Pointers_Problem_Set/isPalindromeValid.java
+++ src/Two_Pointers_Problem_Set/isPalindromeValid.java
@@ -30,4 +30,30 @@ public class isPalindromeValid {
 
         }
 
+        //This  program is with the return value Boolean type.
+        /*
+    public static boolean is_palindrome_valid(String s) {
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            // Skip non-alphanumeric characters from the left.
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+            // Skip non-alphanumeric characters from the right.
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+            // If the characters at the left and right pointers don’t match, the string is
+            // not a palindrome.
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+               return false;
+            }
+            left++;
+            right--;
+        }
+    return true;
+    }
+
+         */
+
 }
