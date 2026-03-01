aeb4bca0940aa221eaf72fd341480151dff5bf5e
Balahari Vignesh <haribala509@gmail.com>
2026-03-01 18:49:41 +0530

    Solved is_palindrome_valid problem from Two Pointers Series

---
+++ src/Two_Pointers_Problem_Set/isPalindromeValid.java
@@ -0,0 +1,33 @@
+package Two_Pointers_Problem_Set;
+
+public class isPalindromeValid {
+    public static void main (String[] args){
+        String val = "abc123";
+
+        is_palindrome_valid(val);
+
+    }
+
+        public static void is_palindrome_valid(String s) {
+            int left = 0, right = s.length() - 1;
+            while (left < right) {
+                // Skip non-alphanumeric characters from the left.
+                while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                    left++;
+                }
+                // Skip non-alphanumeric characters from the right.
+                while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                    right--;
+                }
+                // If the characters at the left and right pointers don’t match, the string is
+                // not a palindrome.
+                if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                     System.out.println("The string is not palindrome!");
+                }
+                left++;
+                right--;
+            }
+
+        }
+
+}
